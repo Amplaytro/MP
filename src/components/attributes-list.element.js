42399a54bc83b295b0678253a3cdaf22eda795b6
Amplaytro <ameyapimpley98@gmail.com>
2022-03-03 18:39:23 +0530

    Uncommented a line

--- src/components/attributes-list.element.js
+++ src/components/attributes-list.element.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { BrowserRouter as Router } from "react-router-dom";
 import { useState } from 'react';
